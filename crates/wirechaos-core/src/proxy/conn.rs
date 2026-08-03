ff54b1659c9a7a758b6cf28cd77b0e007fb99405
sahariardev <rifatsahariar@gmail.com>
2026-08-03 20:21:38 +0600

    feat(startup): wip, promote to tls connection

--- crates/wirechaos-core/src/proxy/conn.rs
+++ crates/wirechaos-core/src/proxy/conn.rs
@@ -1,7 +1,8 @@
 use crate::proxy::buffer_pool::{MultiBufferPool, PooledBytes};
+use crate::proxy::packet::MessageReader;
 use std::sync::Arc;
 use tokio::io;
-use tokio::io::{AsyncReadExt, BufReader, BufWriter};
+use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader, BufWriter};
 use tokio::net::tcp::{OwnedReadHalf, OwnedWriteHalf};
 use tokio::net::TcpStream;
 
@@ -9,9 +10,13 @@ pub struct Conn {
     buffer_reader: BufReader<OwnedReadHalf>,
     buffer_writer: BufWriter<OwnedWriteHalf>,
     pool: Arc<MultiBufferPool>,
+    ssl_done: bool,
 }
 
 const MAX_STARTUP_PACKET_LENGTH: u32 = 10000;
+const CANCEL_REQUEST_CODE: u32 = (1234 << 16) | 5678;
+const SSL_REQUEST_CODE: u32 = (1234 << 16) | 5679;
+const GSSENC_REQUEST_CODE: u32 = (1234 << 16) | 5680;
 
 impl Conn {
     pub fn new(stream: TcpStream, pool: Arc<MultiBufferPool>) -> Self {
@@ -21,6 +26,7 @@ impl Conn {
             buffer_reader: BufReader::new(reader),
             buffer_writer: BufWriter::new(writer),
             pool,
+            ssl_done: false,
         }
     }
 
@@ -28,7 +34,32 @@ impl Conn {
         todo!("handle message")
     }
 
-    pub async fn handle_startup(&self) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn handle_startup(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let buf = self.read_startup_packet().await?;
+        let mut message_reader = MessageReader::new(buf);
+        let protocol_code = message_reader.read_u32()?;
+
+        match protocol_code {
+            SSL_REQUEST_CODE => {
+                if self.ssl_done {
+                    return Err(Box::from("SSL Request is already done"));
+                }
+                //handle ssl request code
+                self.ssl_done = true;
+                self.buffer_writer.write_all(b"S").await?;
+                self.buffer_writer.flush().await?;
+
+                let check = self.buffer_reader.fill_buf().await?;
+
+                if !check.is_empty() {
+                    return Err(Box::from("received unencrypted data after SSL request: possible man-in-the-middle attack"));
+                }
+                //todo:: promote connection to tls connection
+
+            }
+            _ => {}
+        }
+
         todo!("handle startup")
     }
 
@@ -47,7 +78,6 @@ impl Conn {
         Ok(len - 4)
     }
 
-    
     pub async fn read_startup_packet(&mut self) -> Result<PooledBytes, Box<dyn std::error::Error>> {
         let length = self.read_message_length().await?;
 
